dd1d586673445253b2f16c751b8d68938ae605cf
Ying Dong <dongyingname@yahoo.com>
2018-12-14 11:46:08 -0700

    Rename the extension of  all thereact components into .jsx. App.js: import tools from react-router-dom'.

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 
+//import react-router-dom
+// eslint-disable-next-line
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 import './App.scss';
 
 // componets
-
 import Header from './components/Layout/Header';
 import Main from './components/Layout/Main';
 import Footer from './components/Layout/Footer';
+
+
 
 // Apollo client setup
 const client = new ApolloClient({
@@ -18,6 +22,13 @@ const client = new ApolloClient({
 class App extends Component {
   render() {
     return (
+
+      // <BrowserRouter>
+      //   <Route path='/' component = 
+      
+      
+      
+      // </BrowserRouter>
       <ApolloProvider client={client}>
         <div className="App">
           <Header />
