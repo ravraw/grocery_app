06f3ece370bac0cb2ad7998a80e045b458c89448
Ying Dong <dongyingname@yahoo.com>
2018-12-16 18:31:12 -0700

    Department_show: Takes all categories from database; render Category component. Category: Renders products within that category.

--- client/src/components/Layout/Main.jsx
+++ client/src/components/Layout/Main.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 import React, { Component } from "react";
 import Home from "../PageViews/Home";
-// import Department_show from "./components/Department_show";
+import Department_show from "../PageViews/Department_show";
 import User_new from "../PageViews/User_new";
 // import User_show from "./components/User_show";
 // import Login from "./components/Login";
@@ -15,11 +15,15 @@ class Main extends Component {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/user/new" component={User_new} />
-          {/* <Route path="/user/:user_id/show" component={User_show} />
-          <Route path="/department/:department_id/show" component={Department_show} />
-          <Route path="/login" component={Login} />
-          <Route path="/cart" component={Cart} />
-          <Route component={Errors} />  */}
+          <Route
+            path="/department/:department_name/show"
+            component={Department_show}
+          />
+          {/* <Route path="/cart" component={Cart} />
+          <Route component={Errors} />
+          <Route path="/user/:user_id/show" component={User_show} />
+          <Route path="/login" component={Login} /> */}
+
         </Switch>
       </main>
     );
