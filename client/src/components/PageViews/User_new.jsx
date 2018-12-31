8e8eaf9d512902f448c2a2619d0aca9a07f88921
Ying Dong <dongyingname@yahoo.com>
2018-12-30 20:55:37 -0700

    Implementing user registration.

--- client/src/components/PageViews/User_new.jsx
+++ client/src/components/PageViews/User_new.jsx
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class User_new extends Component {
   constructor() {
     super();
     this.state = {
-      username: { value: '', error: false },
-      email: { value: '', error: false },
-      password: { value: '', error: false },
-      confirmPassword: { value: '', error: false }
+      username: { value: "", error: false },
+      email: { value: "", error: false },
+      password: { value: "", error: false },
+      confirmPassword: { value: "", error: false }
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
   handleChange(event) {
     const { value, name } = event.target;
-    if (name !== 'confirmPassword') {
+    if (name !== "confirmPassword") {
       this.setState({
         [name]: { value: value, error: false }
       });
@@ -30,65 +30,90 @@ class User_new extends Component {
     const validEmail = RegExp(
       /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
     );
-
-    for (let name in this.state) {
-      let inputValue = this.state[name].value;
-      let inputLength = inputValue.length;
-      console.log('name', name);
-      if (name === 'username' && inputLength < 9) {
-        console.log('username Error!!!');
-        this.setState({
-          [name]: { value: '', error: true }
-        });
-        console.log('state after email error', this.state);
-      }
-      if (name === 'password' && !validPassword.test(inputValue)) {
-        console.log('Password Error!!!');
-        this.setState({
-          [name]: { value: '', error: true }
-        });
-      }
-      if (name === 'email' && !validEmail.test(inputValue)) {
-        console.log('Email Error!!!');
-
-        this.setState({
-          [name]: { value: '', error: true }
-        });
-      }
-      if (name === 'confirmPassword') {
-        const passwordMatch =
-          this.state.password.value === this.state.confirmPassword.value;
-        console.log('passwordMatch? ', passwordMatch);
-        console.log('this.state', this.state);
-        if (!passwordMatch) {
-          console.log('Confirm password Error!!!');
-          this.setState({
-            [name]: { value: '', error: true }
-          });
-        }
-      }
-    }
     const state = this.state;
-    const usernameError = state.username.error;
-    const emailError = state.email.error;
-    const passwordError = state.password.error;
-    const confirmError = state.confirmPassword.error;
+    let usernameValue = state.username.value;
+    let emailValue = state.email.value;
+    let passwordValue = state.password.value;
+    let confirmValue = state.confirmPassword.value;
+
+    if (usernameValue.length < 9) {
+      console.log(console.log("username Error!!!"));
+      this.setState({ username: { value: "", error: true } });
+    }
+    if (!validEmail.test(emailValue)) {
+      console.log(console.log("email Error!!!"));
+      this.setState({ email: { value: "", error: true } });
+    }
+    if (!validPassword.test(passwordValue)) {
+      console.log(console.log("Password Error!!!"));
+      this.setState({ password: { value: "", error: true } });
+    }
+    const passwordMatch = passwordValue === confirmValue && passwordValue;
+    if (passwordMatch) {
+      console.log(console.log("Confirm-Password Error!!!"));
+      this.setState({ confirmPassword: { value: "", error: true } });
+    }
+
+    let usernameError = state.username.error;
+    let emailError = state.email.error;
+    let passwordError = state.password.error;
+    let confirmError = state.confirmPassword.error;
+
+    // for (let name in this.state) {
+    // let inputValue = this.state[name].value;
+    // let inputLength = inputValue.length;
+    // console.log("name", name);
+    // if (name === "username" && inputLength < 9) {
+    //   console.log("username Error!!!");
+    //   this.setState({
+    //     [name]: { value: "", error: true }
+    //   });
+    //   console.log("state after email error", this.state);
+    // }
+    // if (name === "password" && !validPassword.test(inputValue)) {
+    //   console.log("Password Error!!!");
+    //   this.setState({
+    //     [name]: { value: "", error: true }
+    //   });
+    // }
+    // if (name === "email" && !validEmail.test(inputValue)) {
+    //   console.log("Email Error!!!");
+
+    //   this.setState({
+    //     [name]: { value: "", error: true }
+    //   });
+    // }
+    // if (name === "confirmPassword") {
+    //   const passwordMatch =
+    //     this.state.password.value === this.state.confirmPassword.value &&
+    //     this.state.password.value;
+    //   console.log("passwordMatch? ", passwordMatch);
+    //   console.log("this.state", this.state);
+    //   if (!passwordMatch) {
+    //     console.log("Confirm password Error!!!");
+    //     this.setState({
+    //       [name]: { value: "", error: true }
+    //     });
+    //   }
+    // }
+    // }
+
     const noError =
       !usernameError && !emailError && !passwordError && !confirmError;
-    console.log('state', this.state);
-    console.log('noErrror?', noError);
+    console.log("state", this.state);
+    console.log("noErrror?", noError);
     if (noError) {
-      fetch('http://localhost:4000/register', {
-        method: 'POST',
-        headers: { 'Content-Type': 'text/plain' },
+      fetch("http://localhost:4000/register", {
+        method: "POST",
+        headers: { "Content-Type": "text/plain" },
         body: JSON.stringify({
           username: state.username.value,
           email: state.email.value,
           password: state.password.value
         })
       }).then(response => {
-        console.log('response', response);
-        if (response.ok) console.log('Registraiton Complete!');
+        console.log("response", response);
+        if (response.ok) console.log("Registraiton Complete!");
         //then put user into session and redirect to home page
       });
     }
@@ -105,7 +130,7 @@ class User_new extends Component {
             onChange={this.handleChange}
             placeholder="Email"
           />
-          {this.state.email.error ? <div>Incorrect Email</div> : ''}
+          {this.state.email.error ? <div>Incorrect Email</div> : ""}
 
           <input
             type="text"
@@ -113,7 +138,7 @@ class User_new extends Component {
             onChange={this.handleChange}
             placeholder="Username"
           />
-          {this.state.username.error ? <div>At least 8 charactors!</div> : ''}
+          {this.state.username.error ? <div>At least 8 characters!</div> : ""}
 
           <input
             type="text"
@@ -123,11 +148,11 @@ class User_new extends Component {
           />
           {this.state.password.error ? (
             <div>
-              Need at least 10 characters, one diget, one lower-case letter, and
+              Need at least 10 characters, one digit, one lower-case letter, and
               one upper-case letter!
             </div>
           ) : (
-            ''
+            ""
           )}
 
           <input
@@ -139,7 +164,7 @@ class User_new extends Component {
           {this.state.confirmPassword.error ? (
             <div>Password and confirmPassword don't match!</div>
           ) : (
-            ''
+            ""
           )}
 
           <button className="form_btn" type="submit">
---
+++ client/try.js
@@ -0,0 +1,7 @@
+const arr = [1, 3, 5, 6];
+let num = arr[1];
+num = "hahaha";
+console.log(arr);
+
+arr[1] = "hahaha";
+console.log(arr);
