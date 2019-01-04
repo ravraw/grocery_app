107be129c1a5256d567eeb42119b3f51e91ef71f
Ying Dong <dongyingname@yahoo.com>
2019-01-04 13:34:45 -0700

    Put all the routes into a callback function routes(app)

--- server/src/index.js
+++ server/src/index.js
@@ -20,20 +20,23 @@ app.use(knexLogger(knex));
 
 app.use(require("body-parser").text());
 
-const chargeCallback = require("./callbacks/chargeCallback.js");
-app.post("/charge", async (req, res) => {
-  chargeCallback(req, res);
-});
+const routes = require("./routes.js");
 
-const registerCallback = require("./callbacks/registerCallback.js");
-app.post("/register", async (req, res) => {
-  registerCallback(req, res);
-});
+routes(app);
+// const chargeCallback = require("./callbacks/chargeCallback.js");
+// app.post("/charge", async (req, res) => {
+//   chargeCallback(req, res);
+// });
 
-const distanceCallback = require("./callbacks/distanceCallback.js");
-app.post("/distances", async (req, res) => {
-  distanceCallback(req, res);
-});
+// const registerCallback = require("./callbacks/registerCallback.js");
+// app.post("/register", async (req, res) => {
+//   registerCallback(req, res);
+// });
+
+// const distanceCallback = require("./callbacks/distanceCallback.js");
+// app.post("/distances", async (req, res) => {
+//   distanceCallback(req, res);
+// });
 
 const faker = require("faker");
 const Query = require("./resolvers/Query");
