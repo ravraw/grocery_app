0581fda5b100377ce7675afa5e460eb24059b46a
Ying Dong <dongyingname@yahoo.com>
2019-01-07 14:02:23 -0700

    Redirect to root page if the user tries to refresh on the checkout page

--- client/src/components/PageViews/Checkout.jsx
+++ client/src/components/PageViews/Checkout.jsx
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
-import { graphql } from 'react-apollo';
-import { getCategoryProductsQuery } from '../../queries/queries';
-import { Elements, StripeProvider } from 'react-stripe-elements';
-import CheckoutForm from '../Checkout/CheckoutForm';
+import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import { getCategoryProductsQuery } from "../../queries/queries";
+import { Elements, StripeProvider } from "react-stripe-elements";
+import { NavLink, Link, Redirect } from "react-router-dom";
+
+import CheckoutForm from "../Checkout/CheckoutForm";
 class Checkout extends Component {
   constructor() {
     super();
-    this.state = { date: '', time: '', address: '' };
+    this.state = { date: "", time: "", address: "" };
     this.handleChange = this.handleDateChange.bind(this);
     this.displayTimeSelection = this.displayTimeSelection.bind(this);
   }
@@ -33,67 +35,77 @@ class Checkout extends Component {
   }
   // }
   render(props) {
-    console.log('PROPS FROM CHECKOUT ---', this.props.location.products);
-    const storeName = this.props.location.storeName;
-    const total = Number(this.props.match.params.total);
-    const total1 = total.toFixed(2);
-    const deliveryFee = 5.0;
-    const deliveryFee1 = deliveryFee.toFixed(2);
-    const gst = (total + deliveryFee) * 0.05;
-    const gst1 = gst.toFixed(2);
-    const allTotal = total + gst + deliveryFee;
-    const allTotal1 = allTotal.toFixed(2);
-    const currentDate = new Date();
-    const currentDate1 = currentDate.toISOString().split('T')[0];
-
-    return (
-      <div className="checkout">
-        <h3>Thank you for shopping with us.</h3>
-        <h4>DELIVER DATE: </h4>
-        <input
-          type="date"
-          name="date"
-          id="date"
-          min={currentDate1}
-          onChange={this.handleChange}
+    if (!this.props.location.products) {
+      return (
+        <Redirect
+          to={{
+            pathname: "/"
+          }}
         />
-        <h4>DELIVER Time: </h4>
-        {this.displayTimeSelection()}
-        <h4>BILLING ADDRESS: </h4>
-        <input
-          type="text"
-          id="address"
-          name="address"
-          placeholder="Calgary Address Only"
-          onChange={this.handleChange}
-        />
+      );
+    } else {
+      console.log("PROPS FROM CHECKOUT ---", this.props.location.products);
+      const storeName = this.props.location.storeName;
+      const total = Number(this.props.match.params.total);
+      const total1 = total.toFixed(2);
+      const deliveryFee = 5.0;
+      const deliveryFee1 = deliveryFee.toFixed(2);
+      const gst = (total + deliveryFee) * 0.05;
+      const gst1 = gst.toFixed(2);
+      const allTotal = total + gst + deliveryFee;
+      const allTotal1 = allTotal.toFixed(2);
+      const currentDate = new Date();
+      const currentDate1 = currentDate.toISOString().split("T")[0];
 
-        <p>Please complete the payment</p>
-        <h4>SUBTOTAL: ${total1}</h4>
-        <h4>DELIVERY FEE: ${deliveryFee1}</h4>
-        <h4>GST (5%): ${gst1}</h4>
-        <h4>TOTAL: ${allTotal1}</h4>
-        <h4> </h4>
+      return (
+        <div className="checkout">
+          <h3>Thank you for shopping with us.</h3>
+          <h4>DELIVER DATE: </h4>
+          <input
+            type="date"
+            name="date"
+            id="date"
+            min={currentDate1}
+            onChange={this.handleChange}
+          />
+          <h4>DELIVER Time: </h4>
+          {this.displayTimeSelection()}
+          <h4>BILLING ADDRESS: </h4>
+          <input
+            type="text"
+            id="address"
+            name="address"
+            placeholder="Calgary Address Only"
+            onChange={this.handleChange}
+          />
 
-        <StripeProvider apiKey="pk_test_TYooMQauvdEDq54NiTphI7jx">
-          <div className="example">
-            <Elements>
-              <CheckoutForm
-                products={this.props.location.products}
-                storeName={storeName}
-                deliveryTime={this.state.time}
-                deliveryDate={this.state.date}
-                deliveryAddress={this.state.address}
-                sub_total={total}
-                delivery_charge={deliveryFee}
-                gst_total={gst}
-                total={allTotal}
-              />
-            </Elements>
-          </div>
-        </StripeProvider>
-      </div>
-    );
+          <p>Please complete the payment</p>
+          <h4>SUBTOTAL: ${total1}</h4>
+          <h4>DELIVERY FEE: ${deliveryFee1}</h4>
+          <h4>GST (5%): ${gst1}</h4>
+          <h4>TOTAL: ${allTotal1}</h4>
+          <h4> </h4>
+
+          <StripeProvider apiKey="pk_test_TYooMQauvdEDq54NiTphI7jx">
+            <div className="example">
+              <Elements>
+                <CheckoutForm
+                  products={this.props.location.products}
+                  storeName={storeName}
+                  deliveryTime={this.state.time}
+                  deliveryDate={this.state.date}
+                  deliveryAddress={this.state.address}
+                  sub_total={total}
+                  delivery_charge={deliveryFee}
+                  gst_total={gst}
+                  total={allTotal}
+                />
+              </Elements>
+            </div>
+          </StripeProvider>
+        </div>
+      );
+    }
   }
 }
 
